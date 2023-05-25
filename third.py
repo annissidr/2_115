cdb0e81c459d0ab03c71fd2095f3542ce4052c3e
annissidr <134608866+annissidr@users.noreply.github.com>
2023-05-25 20:12:29 +0300

    Add files via upload

---
+++ third.py
@@ -0,0 +1,22 @@
+import tkinter
+import numpy as np
+def func():
+    x=float(entry_x.get())
+    if(x<0.5):
+        temp=float(x*np.arctan(x))
+        
+    else:
+        temp=float(np.sin(1/x))
+    result.config(text=temp)    
+
+root=tkinter.Tk()
+root.title("third")
+input_x=tkinter.Label(root, text=" Введи x")
+input_x.grid(row=0, column=0)
+entry_x=tkinter.Entry(root)
+entry_x.grid(row=0, column=1)
+button=tkinter.Button(root, text="Подсчет", command=func)
+button.grid(row=1, column=0)
+result=tkinter.Label(root, text="")
+result.grid(row=1, column=1)
+root.mainloop()
